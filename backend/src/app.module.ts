7e915127ea8bce819fd09f7d8b266b32b0338384
Cele :) <cnunez976@alumnos.iua.edu.ar>
2026-05-17 12:44:42 -0300

    Modificacion para el merge

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./modules/auth/auth.module";
+import { UsersModule } from "./modules/users/users.module";
 import { ExpensesModule } from "./modules/expenses/expenses.module";
 import { CategoriesModule } from "./modules/categories/categories.module";
 
@@ -8,6 +9,7 @@ import { CategoriesModule } from "./modules/categories/categories.module";
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+    UsersModule,
     ExpensesModule,
     CategoriesModule,
   ],
